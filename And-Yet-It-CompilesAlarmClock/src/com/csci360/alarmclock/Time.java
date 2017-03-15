9d8be82c19812a7d2d966bd3154a5beb706fabbf
Mikaila Johnson <johnsonnv@g.cofc.edu>
2017-03-15 13:57:48 -0400

    added a skeleton class for time

---
+++ And-Yet-It-CompilesAlarmClock/src/com/csci360/alarmclock/Time.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock;
+
+/**
+ *
+ * @author Nicholas Johnson
+ */
+public class Time {
+    int hour;
+    int minutes;
+    int seconds;
+    
+    public static void setHours(){
+        
+    }
+    
+    public static void setMinutes(){
+        
+    }
+    
+    public static void setSeconds(){
+        
+    }
+    
+    public static int getHours(){
+        return 1; // skeleton
+    }
+    
+    public static int getMinutes(){
+        return 1; // skeleton
+    }
+    
+    public static int getSeconds(){
+        return 1; // skeleton
+    }
+}
