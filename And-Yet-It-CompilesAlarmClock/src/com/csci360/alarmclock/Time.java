2f1c6d7ae8672da47943e7e2a69bde83d92957e2
Mikaila Johnson <johnsonnv@g.cofc.edu>
2017-03-17 12:08:21 -0400

    Cleaned up time and time test

--- And-Yet-It-CompilesAlarmClock/src/com/csci360/alarmclock/Time.java
+++ And-Yet-It-CompilesAlarmClock/src/com/csci360/alarmclock/Time.java
@@ -2,46 +2,91 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
  */
 package com.csci360.alarmclock;
 import java.util.Date;
 /**
  *
- * @author Nicholas Johnson
+ * This class is used to subscribe to the computer clock and
+ * track time so that we can sound the Alarms.
+ * 
+ * @authors Nicholas Johnson and Josh Jettie
  */
 public class Time {
+      
+    // These variables are used to track the computer clock.
     private int hours;
     private int minutes;
     private int seconds;
     
+    
+    /**
+     * set the hours variable and be sure to check the bounds.
+     * @param newHours 
+     */
     public void setHours(int newHours){
-       if(newHours >= 61 || newHours < 0){
-           //System.out.println("Incorrect Hours");
-       }
-       else{
-        hours = newHours;
-    }
+        
+        if(newHours >= 60 || newHours < 0){
+           // TODO throw an error.
+        }else{
+           hours = newHours;
+        }
     }
     
+    /**
+     * set the minutes variable and be sure to check bounds.
+     * @param newMinutes 
+     */
     public void setMinutes(int newMinutes){
-        minutes = newMinutes;
+        
+        if(newMinutes >= 60 || newMinutes < 0){
+           // TODO throw an error.
+        }else{
+           hours = newMinutes;
+        }
     }
     
+    /**
+     * set the seconds variable and be sure to check bounds.
+     * @param newSeconds 
+     */
     public void setSeconds(int newSeconds){
-        seconds = newSeconds;
+        
+        if(newSeconds >= 60 || newSeconds < 0){
+           // TODO throw an error.
+        }else{
+           hours = newSeconds;
+        }
     }
     
+    /**
+     * 
+     * @return hours 
+     */
     public int getHours(){
-        return hours; // skeleton
+        return hours;
     }
     
+    /**
+     * 
+     * @return minutes
+     */
     public int getMinutes(){
-        return minutes; // skeleton
+        return minutes;
     }
     
+    /**
+     * 
+     * @return seconds
+     */
     public int getSeconds(){
-        return seconds; // skeleton
+        return seconds;
     }
+    
+    /**
+     * set all time variables to the time of the computer clock.
+     */
     public void updateTime(){
         
        Date date = new Date();
@@ -49,15 +94,5 @@ public class Time {
        setMinutes(date.getMinutes());
        setSeconds(date.getSeconds());
 
-        
-        
-      
-    }
-
-
-
-    
-    
-    
-    
+    } 
 }
--- And-Yet-It-CompilesAlarmClock/test/com/csci360/alarmclock/TimeTest.java
+++ And-Yet-It-CompilesAlarmClock/test/com/csci360/alarmclock/TimeTest.java
@@ -41,93 +41,10 @@ public class TimeTest {
     public void testUpdateTime() {
         System.out.println("Test UpdateTime");
         Time instance = new Time();
+        Date date = new Date();
         instance.updateTime();
+        int hours = date.getHours();
+        assertEquals(instance.getHours(),hours);
     } 
-
-    /**
-     * Test of getHours method, of class Time.
-     */
-    @Test
-    public void testGetHours() {
-        System.out.println("getHours");
-        Time instance = new Time();
-        Date comp = new Date();
-        instance.updateTime();
-        int expResult = comp.getHours();
-        int result = instance.getHours();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getMinutes method, of class Time.
-     */
-    @Test
-    public void testGetMinutes() {
-        System.out.println("getMinutes");
-        Time instance = new Time();
-        Date comp = new Date();
-        instance.updateTime();
-        int expResult = comp.getMinutes();
-        int result = instance.getMinutes();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getSeconds method, of class Time.
-     */
-    @Test
-    public void testGetSeconds() {
-        System.out.println("getSeconds");
-        Time instance = new Time();
-        Date comp = new Date();
-        instance.updateTime();
-        int expResult = comp.getSeconds();
-        int result = instance.getSeconds();
-        assertEquals(expResult, result);
-    }
-    /**
-     * Test of setHours method, of class Time.
-     */
-    @Test
-    public void testSetHours() {
-        System.out.println("setHours");
-        int newHours = 13;
-        Time instance = new Time();
-        instance.setHours(newHours);
-        assertEquals(instance.getHours(), newHours);
-    }
-        @Test
-    public void testSetIncorrectHours() {
-        System.out.println("SetIncorrectHours");
-        int newHours = 90;
-        Time instance = new Time();
-        instance.setHours(newHours);
-        assertEquals("Incorrect Hours", newHours);
-    }
-
-    /**
-     * Test of setMinutes method, of class Time.
-     */
-    @Test
-    public void testSetMinutes() {
-        System.out.println("setMinutes");
-        int newMinutes = 13;
-        Time instance = new Time();
-        instance.setMinutes(newMinutes);
-        assertEquals(instance.getMinutes(), newMinutes);
-    }
-
-    /**
-     * Test of setSeconds method, of class Time.
-     */
-    @Test
-    public void testSetSeconds() {
-        System.out.println("setSeconds");
-        int newSeconds = 13;
-        Time instance = new Time();
-        instance.setSeconds(newSeconds);
-        assertEquals(instance.getSeconds(), newSeconds);
-    } 
-
 }
 
